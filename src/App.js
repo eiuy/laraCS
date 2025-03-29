e187ba3155bd38cacf28cae5320cd362d5dc7d5d
EIUY <eiuy@Ls-MacBook-Pro.local>
2025-03-30 00:56:08 +0800

    Initial real deploy

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MoneyboxCTF from "./pages/MoneyboxCTF";
+
+function App() {
+  return (
+    <div>
+      <MoneyboxCTF />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/MoneyboxCTF.jsx
@@ -0,0 +1,1 @@
+[canvas content here will be inserted in next step]
